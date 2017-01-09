4e9c69e621b31fef591a7f094e9262efacb30b52
Rahul Kumar <rahul@thoughtnirvana.com>
2017-01-10 03:08:12 +0530

    Bug fix.

--- max_slice_sum.rb
+++ max_slice_sum.rb
@@ -1,7 +1,7 @@
 def solution(a)
   max_ending_here, max_slice = a[0], a[0]
   a[1..-1].each do |el|
-    max_ending_here = [el, max_ending_here].max
+    max_ending_here = [el, max_ending_here + el].max
     max_slice = [max_slice, max_ending_here].max
   end
   max_slice
